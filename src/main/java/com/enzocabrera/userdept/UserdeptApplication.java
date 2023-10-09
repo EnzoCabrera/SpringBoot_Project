dd497e055c39667f1f2fa9319aa09f2d05adf50b
EnzoCabrera <enzo2005cabrera@gmail.com>
2023-10-09 14:28:15 -0300

    Starting project

---
+++ src/main/java/com/enzocabrera/userdept/UserdeptApplication.java
@@ -0,0 +1,13 @@
+package com.enzocabrera.userdept;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdeptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserdeptApplication.class, args);
+	}
+
+}
